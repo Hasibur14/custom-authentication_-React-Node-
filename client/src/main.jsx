7a3bc5928c97ae05943aa0718cc3f8963a1d1639
hasibur14 <hasib141415@gmail.com>
2024-08-14 18:44:24 +0600

    added: register and login

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from "./page/home/Home";
+import Login from "./page/Login";
+import Register from "./page/Register";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
---
+++ client/src/page/home/Home.jsx
@@ -0,0 +1,13 @@
+
+
+const Home = () => {
+    return (
+        <div className="container mx-auto my-16">
+            <h2>  Welcome to this site...</h2>
+
+            <link className="btn bg-gray-300 rounded p-2">LOGIN</link>
+        </div>
+    );
+};
+
+export default Home;
